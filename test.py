ff341ff06570b51d57feeb1439b5b669f4919509
Atipat Pankong <p.atipat@ext-lmwn.com>
2023-02-10 02:20:56 +0700

    ingredient data

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import re
-
-def remove_emojis(data):
-    return  ''.join(re.findall(r"[\u0E00-\u0E7Fa-zA-Z\.']", data))
-
-text = 'สวัสดี Hello 123456.'
-
-print(remove_emojis(text))

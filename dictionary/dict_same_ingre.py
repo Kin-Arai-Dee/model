57aa21359725b94a506c67d954245d788e73d593
Atipat Pankong <p.atipat@ext-lmwn.com>
2023-02-12 22:26:12 +0700

    sent data to eakky

--- dictionary/dict_same_ingre.py
+++ dictionary/dict_same_ingre.py
@@ -1,4 +1,43 @@
+import sys
+import os
+
+# in jupyter (lab / notebook), based on notebook path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from all_ingredients_unit_map import all_ingredients_unit_map
+
 a = {
   'ยากิโชบะรสโชยุ':'ยากิโชบะ',
   'ยากิโชบะ': 'ยากิโชบะ',
+  'ไข่ไก่สด': 'ไข่ไก่',
+  'ไข่ไก่สด อนามัย': 'ไข่ไก่',
+  'ไข่ไก่สำหรับทอดไข่ดาว': 'ไข่ไก่',
+  'ไข่ไก่สำหรับผัด': 'ไข่ไก่',
+  'ไข่ไก่หรือไข่เป็ดก็ได้ แล้วแต่ความชอบ': 'ไข่ไก่',
+  'ไข่ไก่เต็มใบ': 'ไข่ไก่',
+  'ไข่ไก่เบอรใหญ่': 'ไข่ไก่',
+  'ไข่ไก่เบอร์ 0': 'ไข่ไก่',
+  'ไข่ไก่เบอร์ 1': 'ไข่ไก่',
+  'ไข่ไก่เบอร์ 1-2': 'ไข่ไก่',
+  'ไข่ไก่เบอร์ 2': 'ไข่ไก่',
+  'ไข่ไก่เบอร์ 3': 'ไข่ไก่',
+  'ไข่ไก่เบอร์0': 'ไข่ไก่',
+  'ไข่ไก่เบอร์1': 'ไข่ไก่',
+  'ไข่ไก่เบอร์2': 'ไข่ไก่',
+  'ไข่ไก่เบอร์3': 'ไข่ไก่',
+  'ไข่ไก่เบอร์3-4': 'ไข่ไก่',
 }
+
+tmp_dict = {}
+
+if __name__ == '__main__':
+  for key in all_ingredients_unit_map.keys():
+    if key in a:
+      new_key = a[key]
+    else:
+      continue
+    if new_key not in tmp_dict:
+      tmp_dict[new_key] = set()
+    tmp_dict[new_key].update(all_ingredients_unit_map[key])
+
+print(tmp_dict)
--- dictionary/regex_util.py
+++ dictionary/regex_util.py
@@ -1,11 +1,16 @@
-clean_char_regex = r'[^\w\s\(\)\{\}\[\]\/\u0E00-\u0E7F]+|_'
+clean_char_regex = r'[^\w\s\(\)\{\}\[\]\/\u0E00-\u0E7F]+|_' # \t \n all special char except \ / _ { } () []
 
-space_regex = r'\s+'
+space_regex = r'\s+' # '         ' , ' '
 
-number_regex = r'[\d]+'
+number_regex = r'[\d]+' # 1, 12 , 434
 
-number_with_special_character_regex = r'((?:[\d\u00BC-\u00BE\u2150-\u215E]+(?:\s*[\.\/]\s*\d+)?[\u00BC-\u00BE\u2150-\u215E\+]*)|(?:[\+\-]))'
+number_with_special_character_regex = r'((?:[\d\u00BC-\u00BE\u2150-\u215E]+(?:\.\d+)?(?:\s*[\/]\s*\d+)?[\u00BC-\u00BE\u2150-\u215E]*)|(?:[\+\-\*])|(?:\.\d+))' 
 
 fraction_regex =r'[\u00BgC-\u00BE\u2150-\u215E]+' # match ½
 
-num_on_start_and_in_parentheses = r"^\d+[.)|\s]*"
+num_on_start_and_in_parentheses = r"^\d+[.)|\s]*" # prefix 1) 1. 1).
+
+match_in_parentheses = r'\(([^)]+)\)' # (1212)
+
+match_or_word = r'หรือ|\(|\)' # หรือ,(,)
+match_phamand_word = r'ประมาณ|\(|\)' # ประมาณ,(,)
